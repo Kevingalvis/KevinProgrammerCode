2b09879e7c5efda4d7360b01f90c12616c7ac024
kevin galvis <kevin_david_t@outlook.com>
2023-11-13 22:21:28 -0500

    boton theme dia/noche

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
 import {barraNavegacion} from "./component/barraNavegacion.js";
 import {barraRedes} from "./component/barra_redes_sociales.js";
+
 /** ejecucion del componente */
 window.customElements.define("barra-navegacion", barraNavegacion);
 window.customElements.define("barra-redes", barraRedes);
+
+/** variables */
+const $btnTheme = document.querySelectorAll('#navTheme a'); /** Variable para el modo oscuro y dia */
+
+/** esto hace que aparezca el modo oscuro y dia */
+$btnTheme.forEach((btnTheme) => {
+    btnTheme.addEventListener('click', (e) =>{
+        /** obtenemos el valor id de los iconos que van hacer las dos llaves */
+        let themeKey = e.target.id;
+        switch (themeKey) {
+            /** lo que hacemos es agregar y quitar la clase (navTheme_disabled), que es la que desactiva el icono para que aparezca el otro
+             * en esta primera parte lo que hace es quitar el icono (SUN) para activar el modo oscuro
+            */
+            case 'icon-theme-moon':
+                document.getElementById('switch-theme-sun').classList.toggle('navTheme_disabled');
+                document.getElementById('switch-theme').classList.remove('navTheme_disabled');
+                break;
+            
+            case 'icon-theme-sun':
+                
+            /** lo que hacemos es agregar y quitar la clase (navTheme_disabled), que es la que desactiva el icono para que aparezca el otro
+             * en esta primera parte lo que hace es quitar el icono (Moon) para activar el modo claro
+            */
+                document.getElementById('switch-theme').classList.toggle('navTheme_disabled');
+                document.getElementById('switch-theme-sun').classList.remove('navTheme_disabled');
+            break;
+        
+            default:
+                break;
+        }
+
+
+    });
+});
+
 
--- js/component/barraNavegacion.js
+++ js/component/barraNavegacion.js
@@ -11,18 +11,24 @@ export class barraNavegacion extends HTMLElement{
                 <div class="navLogo">
                     <img src="../../assets/logo/logoColor.svg" />
                 </div>
-                <div class="navLinks">
+                <div class="navLinks" id="y">
                     <a href="https://www.mozilla.com/"><i class="fa-solid fa-user-tie"></i>Perfil</a>
                     <a href="https://www.mozilla.com/"><i class="fa-solid fa-screwdriver-wrench"></i>Projectos</a>
                     <a href="https://www.mozilla.com/"><i class="fa-solid fa-envelope"></i>Contactame</a>
                 </div>
-                <div class="navTheme">
-                    <a><i class="fa-solid fa-cloud-moon"></i></a>
+                <div class="navTheme" id="navTheme">
+                    <a id="switch-theme" class=""><i class="fa-solid fa-cloud-moon" id="icon-theme-sun"></i></a>
+                    <a id="switch-theme-sun" class="navTheme_disabled"><i class="fa-solid fa-cloud-sun" id="icon-theme-moon"></i></a>
+                  
                 </div>
 
             </nav>
         </header>`;
+
        
     }
+    
+    
+
 }
 
